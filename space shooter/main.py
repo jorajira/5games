98410e078f4c146ac776dd4e0b7b98debd4d489d
Jorah <jorahdumond@gmail.com>
2025-05-29 16:57:08 +0300

    fixed spaceship not moving

--- space shooter/main.py
+++ space shooter/main.py
@@ -1,6 +1,7 @@
 import pygame;
 from os.path import join
 from random import randint
+
 # Do a general setup.  Initialize pygame.
 pygame.init();
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720;
@@ -19,6 +20,8 @@ path = join('images', 'player.png')
 print(path)
 player_surf = pygame.image.load(join('space shooter', 'images', 'player.png')).convert_alpha()
 star_surf = pygame.image.load(join('space shooter', 'images', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
 #Make sure window stays open using a while loop.
 while running:
     # event loop
@@ -29,12 +32,13 @@ while running:
     # draw the game
     # fill the window with red color
     display_surface.fill("darkgray")
-    x += 0.1
+    
     # attaches surf to window, first number is from the left, height is from top left corner.
-    display_surface.blit(player_surf, (100,150))
     #generate 20 stars on the screen at a time
-    for i in range(20):
-        display_surface.blit(star_surf, (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
+    x += 0.1
+    display_surface.blit(player_surf, (x,150))
     #this puts the game on the screen
     pygame.display.flip() #update and flip do the same thing.
     
